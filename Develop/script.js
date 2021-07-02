cb18c7574252905cf943b4c8af7b125ddca88918
sxwqsd <mercado.valeriai856@gmail.com>
2021-07-01 22:24:39 -0700

    i added arrays for a=most of the variables

--- Develop/script.js
+++ Develop/script.js
@@ -4,16 +4,17 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
-var upper;
-var lower;
-var numeric;
-var special;
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numeric= ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var special = ['!', '#', '$', '&', '%'];
 var userChoices;
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
@@ -32,17 +33,17 @@ function confirmUpper(){
 
 }
 function confirmNumeric(){
-  numeric = confirm('Do you want upper case letters in your password?')
+  numeric = confirm('Do you want numbers in your password?')
   console.log(numeric)
 
 }
 function confirmLower(){
-  lower = confirm('Do you want upper case letters in your password?')
+  lower = confirm('Do you want lower case letters in your password?')
   console.log(lower)
 
 }
 function confirmSpecial(){
-  special = confirm('Do you want upper case letters in your password?')
+  special = confirm('Do you want special charaters in your password?')
   console.log(special)
 
 }
@@ -54,7 +55,11 @@ function generatePassword(){
   confirmSpecial()
 }
 
+var password = upper.concat(lower, numeric, special)
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
 
