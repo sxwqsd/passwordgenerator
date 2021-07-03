86de9b1ab0b93d31afe22c0afebffd744f3d9dad
sxwqsd <mercado.valeriai856@gmail.com>
2021-07-03 12:32:01 -0700

    got it to randomize which user choices it uses instead of doing a pattern of all of them

--- Develop/script.js
+++ Develop/script.js
@@ -6,113 +6,161 @@ var generateBtn = document.querySelector("#generate");
 var passwordLength;
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numeric= ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var special = ['!', '#', '$', '&', '%'];
 var result = "";
 
 var userChoices = {
-  
 }
+
+var userChoicesArr = []
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
 
   passwordText.value = password;
+
 
 }
 
 generateBtn.addEventListener("click", writePassword)
 
-function confirmPasswordLength(){
-   passwordLength = prompt('Choose a password length of at least 8 characters but no more than 128 charaters')
-   console.log(passwordLength)
-  if (passwordLength <8||passwordLength >128){
+function confirmPasswordLength() {
+  passwordLength = prompt('Choose a password length of at least 8 characters but no more than 128 charaters')
+  console.log(passwordLength)
+  if (passwordLength < 8 || passwordLength > 128) {
     window.alert('Invalid Input')
     confirmPasswordLength()
   }
 }
-function confirmUpper(){
+function confirmUpper() {
   userChoices.upperChoice = confirm('Do you want upper case letters in your password?')
-  console.log(userChoices.upperChoice)
+  if (userChoices.upperChoice) {
+    userChoicesArr.push("upper")
+  }
+  console.log(userChoices.upperChoice);
 
 }
-function confirmNumeric(){
+function confirmNumeric() {
   userChoices.numericChoice = confirm('Do you want numbers in your password?')
-  console.log(userChoices.numericChoice)
+  if (userChoices.numericChoice) {
+    userChoicesArr.push("numeric")
+  }
+  console.log(userChoices.numericChoice);
 
 }
-function confirmLower(){
+function confirmLower() {
   userChoices.lowerChoice = confirm('Do you want lower case letters in your password?')
-  console.log(userChoices.lowerChoice)
+  if (userChoices.lowerChoice) {
+    userChoicesArr.push("lower")
+  }
+  console.log(userChoices.lowerChoice);
 
 }
-function confirmSpecial(){
+function confirmSpecial() {
   userChoices.specialChoice = confirm('Do you want special charaters in your password?')
-  console.log(userChoices.specialChoice)
+  if (userChoices.specialChoice) {
+    userChoicesArr.push("special")
+  }
+  console.log(userChoices.specialChoice);
 
 }
-function generatePassword(){
-  console.log(userChoices); 
+function generatePassword() {
   confirmPasswordLength()
   confirmUpper()
   confirmLower()
   confirmNumeric()
   confirmSpecial()
 
-  console.log(userChoices);
-
-  if(!upper&&!lower&&!numeric&&!special){
-    userChoices = alert("You must choose a criteria");
-  
+  if (!upper && !lower && !numeric && !special) {
+    alert("You must choose a criteria");
   }
 
-  for (var i = 0; 1 < passwordLength; i++){
-    result += userChoices.concat(Math.floor(Math.random() * userChoices.passwordLength));
+  /*userChoices = {
+    upperChoice: true,
+    lowerChoice: true,
+    numericChoice: true,
+    specialChoice: true
+  }
 
-    if(userChoices.upperChoice){
-      var upperIndex =  Math.floor(Math.random() * upper.length);
-      var upperResult = upper[upperIndex];
-      result += upperResult;
-      console.log(result);
-     
-    }
-    if(lower){
-      userChoices += lower
-      console.log(userChoices);
-    }
-  
-    if(numeric) {
-      userChoices += numeric
-      console.log(userChoices);
-    }
-  
-    if(special) {
-      userChoices += special
-      console.log(userChoices);
+  userChoicesArr = [
+    "upper",
+    "lower",
+    "numeric",
+    "speciaChoice"
+  ]
+  */
+ console.log(userChoicesArr)
+  for (var i = 0; i < passwordLength; i++) {
+    //result += userChoices(Math.floor(Math.random() * userChoices.passwordLength));
+
+    switch (userChoicesArr[Math.floor(Math.random() * userChoicesArr.length)]) {
+      case "upper":
+        var upperIndex = Math.floor(Math.random() * upper.length);
+        var upperResult = upper[upperIndex];
+        result += upperResult;
+        console.log(result);
+        break;
+
+      case "lower":
+        var lowerIndex = Math.floor(Math.random() * lower.length);
+        var lowerResult = lower[lowerIndex];
+        result += lowerResult;
+        console.log(result);
+        break;
+
+      case "numeric":
+        var numericIndex = Math.floor(Math.random() * numeric.length);
+        var numericResult = numeric[numericIndex];
+        result += numericResult;
+        console.log(result);
+        break;
+
+      case "special":
+        var specialIndex = Math.floor(Math.random() * special.length);
+        var specialResult = special[specialIndex];
+        result += specialResult;
+        console.log(result);
+        break;
+
+
+      default:
+        break;
     }
 
+    // if (userChoices.upperChoice) {
+    //   var upperIndex = Math.floor(Math.random() * upper.length);
+    //   var upperResult = upper[upperIndex];
+    //   result += upperResult;
+    //   console.log(result);
+    // }
+    // if (userChoices.lowerChoice) {
+    //   var lowerIndex = Math.floor(Math.random() * lower.length);
+    //   var lowerResult = lower[lowerIndex];
+    //   result += lowerResult;
+    //   console.log(result);
+    // }
+
+    // if (userChoices.numericChoice) {
+    //   var numericIndex = Math.floor(Math.random() * numeric.length);
+    //   var numericResult = numeric[numericIndex];
+    //   result += numericResult;
+    //   console.log(result);
+    // }
+
+    // if (userChoices.specialChoice) {
+    //   var specialIndex = Math.floor(Math.random() * special.length);
+    //   var specialResult = special[specialIndex];
+    //   result += specialResult;
+    //   console.log(result);
+    // }
 
   }
   return result
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
